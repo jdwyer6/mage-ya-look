bbdaeda1dee54582d37b9892a383e085e1ec70de
kegcheck <65734212+kegcheck@users.noreply.github.com>
2023-07-13 12:40:55 -0500

    add to randomPDP

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
 document.getElementById('random-pdp-btn').addEventListener('click', getRandomPDP);
 
 function getRandomPDP() {
-    chrome.tabs.create({ url: 'https://www.google.com' });
+    let randomProductIndex = getRandomInt(0, products.length - 1);
+    let randomUrl = products[randomProductIndex].__5;
+    window.location.href = randomUrl;
 }
 
-console.log(products[0])
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(products[getRandomInt(0, products.length - 1)].__5);

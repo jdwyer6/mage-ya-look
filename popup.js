672a111ce32ba2060af6bd6e7be413adde01287e
kegcheck <65734212+kegcheck@users.noreply.github.com>
2023-07-13 19:43:20 -0500

    fix error with randompdp and add input fields

--- popup.js
+++ popup.js
@@ -1,9 +1,20 @@
+
+let urlHttps = document.getElementById("urlHttps")
+let urlBase = document.getElementById("urlBase")
+let urlDisplay = document.getElementById('urlDisplay')
+
+updateCurrentUrl();
+
+urlHttps.addEventListener('input', updateCurrentUrl);
+urlBase.addEventListener('input', updateCurrentUrl);
+
 document.getElementById('random-pdp-btn').addEventListener('click', getRandomPDP);
 
 function getRandomPDP() {
     let randomProductIndex = getRandomInt(0, products.length - 1);
     let randomUrl = products[randomProductIndex].__5;
-    window.location.href = randomUrl;
+  chrome.tabs.create({ url: `${urlDisplay.innerHTML}${randomUrl}` });
+  
 }
 
 function getRandomInt(min, max) {
@@ -12,4 +23,29 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(products[getRandomInt(0, products.length - 1)].__5);
+function updateCurrentUrl() {
+    urlDisplay.innerHTML = `${urlHttps.value}${urlBase.value}`;
+}
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const tab = tabs[0];
+    const tabUrl = tab.url;
+  
+    // Send a HEAD request to the URL to get the HTTP status code
+    fetch(tabUrl, { method: 'HEAD' })
+      .then(response => {
+        const statusCode = response.status;
+        if (statusCode === 404) {
+          console.log('Current page is a 404');
+        } else {
+          // Handle other cases
+          console.log('Current page is not a 404');
+        }
+      })
+      .catch(error => {
+        // Handle fetch error
+        console.error('Failed to fetch the page:', error);
+      });
+  });
+  
+
